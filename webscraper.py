eada65143eab18b6d87865a518f4f40fca088d00
Eric Sun <suneric98@gmail.com>
2020-01-02 16:36:44 -0500

    better data source

--- webscraper.py
+++ webscraper.py
@@ -2,6 +2,9 @@ import praw
 import datetime as dt
 import pandas as pd
 import re
+import requests
+import json
+import time
 
 RE_EMOJI = re.compile(u'([\U00002600-\U000027BF])|([\U0001f300-\U0001f64F])|([\U0001f680-\U0001f6FF])')
 
@@ -22,12 +25,37 @@ def scrape_subreddit(sub):
     top = subreddit.top(limit=500)
 
     data = {"titles":[],"texts":[]}
+    count = 0
     for submission in top:
         if re.search(RE_EMOJI, submission.selftext):
-            print(submission.title, submission.selftext)
             data["titles"].append(submission.title)
             data["texts"].append(submission.selftext)
+            count += 1
+    print("Amount of data gathered:{}".format(count))
     return pd.DataFrame(data)
 
-df = scrape_subreddit("copypasta")
-df.to_csv("data.csv",index=False)
+def scrape_subreddit_pushshift(sub, day = 10, size = "100"):
+    data = {"titles":[], "texts":[]}
+    hashedTitles = set()
+    while day > 0:
+        time.sleep(2)
+        url = "https://api.pushshift.io/reddit/search/submission/?subreddit=" + sub + "&after=" + str(day) + "d&size=" + size
+        resp = requests.get(url)
+        jsonData = resp.json()["data"]
+        count = 0
+        for submission in jsonData:
+            if "selftext" not in submission:
+                continue
+            if re.search(RE_EMOJI, submission["selftext"]) and submission["title"] not in hashedTitles:
+                data["titles"].append(submission["title"])
+                data["texts"].append(submission["selftext"])
+                hashedTitles.add(submission["title"])
+                count += 1
+        day -= 1
+        print("Day:{}, Amount of data gathered:{}".format(day, count))
+    print("Total data size:{}".format(len(data["titles"])))
+    return pd.DataFrame(data)
+
+# df = scrape_subreddit("copypasta")
+df = scrape_subreddit_pushshift("copypasta", 50, "100")
+df.to_csv("data2.csv",index=False)
